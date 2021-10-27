44e36da381b1506735d170d3fe515c2c85613376
barcenas.proteco@gmail.com <Edgar1515>
2021-10-27 04:40:44 -0500

    Retrofit with Coroutines

---
+++ app/src/main/java/com/dan/retrofitcoroutinas/DogResponse.kt
@@ -0,0 +1,9 @@
+package com.dan.retrofitcoroutinas
+
+import com.google.gson.annotations.SerializedName
+
+data class DogsResponse(
+    @SerializedName("status") var status: String,
+    @SerializedName("message") var images: List<String>
+)
+
